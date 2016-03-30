4bb6e155788a7e650ceefd6ae328b3bd4ddad09c
Yaroslav_Strontsitskyy <Yaroslav_Strontsitskyy@epam.com>
2016-03-30 14:29:33 +0300

    resolved issue with context duplication

--- Theater/src/main/java/com/epam/movies/app/Application.java
+++ Theater/src/main/java/com/epam/movies/app/Application.java
@@ -1,14 +1,13 @@
 package com.epam.movies.app;
 
-import com.epam.movies.configuration.ApplicationConfig;
 import com.epam.movies.configuration.MovieTheaterInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 
 public class Application {
 
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(ApplicationConfig.class).child(MovieTheaterInitializer.class).run(args);
-	}
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(MovieTheaterInitializer.class).run(args);
+    }
 
 }
--- Theater/src/main/java/com/epam/movies/configuration/WebConfig.java
+++ Theater/src/main/java/com/epam/movies/configuration/WebConfig.java
@@ -1,9 +1,21 @@
 package com.epam.movies.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import com.epam.movies.service.UserDetailsServiceImpl;
+import com.epam.movies.util.ProcessExecutor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.*;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -12,9 +24,116 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.ResourceBundleViewResolver;
 
-@Configuration
-@ComponentScan(basePackages = {"com.epam.movies.controller"})
+import javax.annotation.PostConstruct;
+import javax.servlet.MultipartConfigElement;
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+@ComponentScan(basePackages = "com.epam.movies.*")
+@EnableAspectJAutoProxy
+@PropertySources({@PropertySource("classpath:auditoriums.properties"),
+        @PropertySource("classpath:environment.properties")})
 public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Value("${db.host}")
+    private String dbHost;
+
+    @Value("${db.name}")
+    private String dbName;
+
+    @Value("${db.port}")
+    private String port;
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
+    @Value("${db.restoreFromDump}")
+    private boolean restoreFromDump;
+
+    @Value("${db.dumpName}")
+    private String dumpFileName;
+
+    @Value("${db.serverLocationFolder}")
+    private String serverLocationFolder;
+
+    private static final String configFileName = "environment.properties";
+
+    @PostConstruct
+    public void initDataBase() throws IOException {
+        if (restoreFromDump) {
+            Resource resource = new ClassPathResource(dumpFileName);
+            File dumpFile = resource.getFile();
+            boolean isDbRestoringSuccessful = restoreDB(serverLocationFolder, dbHost, dbName, username, password,
+                    dumpFile.getAbsolutePath());
+            if (isDbRestoringSuccessful) {
+                Resource envConfigFileResource = new ClassPathResource(configFileName);
+                File envConfigFile = envConfigFileResource.getFile();
+                Properties properties = new Properties();
+                properties.load(new FileInputStream(envConfigFile));
+                properties.replace("db.restoreFromDump", "false");
+                properties.store(new FileOutputStream(envConfigFile), "changed init param");
+            }
+        }
+
+    }
+
+    public static boolean restoreDB(String serverLocationFolder, String dbHost, String dbName, String dbUserName, String dbPassword,
+                                    String source) throws IOException {
+
+        Resource restoreDbBatFileResource = new ClassPathResource("restoreDb.bat");
+        File restoreDbBatFile = restoreDbBatFileResource.getFile();
+
+        ProcessBuilder restoreDbFromFile = new ProcessBuilder("cmd", "/c", "start", restoreDbBatFile.getAbsolutePath(),
+                serverLocationFolder, dbHost, dbUserName, dbPassword, dbName, source);
+
+        boolean dbRestored = ProcessExecutor.executeProcess(restoreDbFromFile, "db restored",
+                "fail while restoring db");
+        return dbRestored;
+    }
+
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl(
+                "jdbc:mysql://" + dbHost + ":" + port + "/" + dbName + "?autoReconnect=true&useSSL=false&create=true");
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        return factory;
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("128KB");
+        factory.setMaxRequestSize("128KB");
+        return factory.createMultipartConfig();
+    }
 
 
     @Override
